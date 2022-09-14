e73cb483d4751782e1a85fb5d712d5f65b6d8503
ywil0002 <55167229+ywil0002@users.noreply.github.com>
2022-09-14 12:24:05 +1000

    Wk 7

---
+++ Wk7 Lab/vega_lite_vis.js
@@ -0,0 +1,6 @@
+var vg_1 = "bar_chart.vg.json";
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+    // Access the Vega view instance
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
